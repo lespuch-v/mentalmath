b8f6e7a73ab046bbd11cc2d6cdf555892bab88fd
lespuch-v <v.lespukh@gmail.com>
2024-09-28 12:02:25 +0200

    ui update

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from './navbar/navbar.component';
+import { QuickMathComponent } from "./quick-math/quick-math.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NavbarComponent],
+  imports: [RouterOutlet, NavbarComponent, QuickMathComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
